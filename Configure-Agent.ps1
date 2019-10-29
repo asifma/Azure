b20d4d0b82bc6752194ccc02205461757deffe6b
Asif Mithawala <asifm@live.se>
2019-10-29 19:32:52 +0100

    Update Configure-Agent.ps1

--- Configure-Agent.ps1
+++ Configure-Agent.ps1
@@ -8,14 +8,3 @@ Param(
 
 $newdisk = @(get-disk | Where-Object partitionstyle -eq 'raw')
 $Labels = @('agentdisk','Backup','Data','System','Logs')
-
-for($i = 0; $i -lt $newdisk.Count ; $i++)
-{
-
-    $disknum = $newdisk[$i].Number
-    $dl = get-Disk $disknum | 
-       Initialize-Disk -PartitionStyle GPT -PassThru | 
-          New-Partition -AssignDriveLetter -UseMaximumSize
-    Format-Volume -driveletter $dl.Driveletter -FileSystem NTFS -NewFileSystemLabel $Labels[$i] -Confirm:$false
-
-}
