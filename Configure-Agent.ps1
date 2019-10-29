15dab84e706aa354ab274f145323ea1aa30d13f3
Asif Mithawala <asifm@live.se>
2019-10-29 18:58:12 +0100

    Update Configure-Agent.ps1

--- Configure-Agent.ps1
+++ Configure-Agent.ps1
@@ -1,3 +1,11 @@
+Param(
+  [Parameter(Mandatory)]
+  [string] $TeamAccount,
+  [string] $PoolName = "default",
+  [Parameter(Mandatory)]
+  [string] $PATToken
+)
+
 $newdisk = @(get-disk | Where-Object partitionstyle -eq 'raw')
 $Labels = @('agentdisk','Backup','Data','System','Logs')
 
